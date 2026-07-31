0b17fbb2734f352f4a82cb3cb0879fb56563e9e6
MichaelCorduroy <cellben99@gmail.com>
2026-07-30 23:57:36 -0400

    Expanded endpoints for library API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,19 @@
 import Fastify from "fastify";
 import {scanDirectory} from "./scanner";
+import fs from "node:fs";
 import db from "./database";
 
 const app = Fastify();
+import cors from "@fastify/cors";
+
+
+async function start() {
+	
+
+await app.register(cors, {
+	origin: true 
+});
+
 
 
 app.get("/scan", async () => {
@@ -17,6 +28,34 @@ app.get("/songs", async () => {
 });
 
 
+app.get("/stream/:id", async (request, reply) => {
+	const { id } = request.params as { id: string };
+
+	const song = db.prepare(`SELECT path FROM songs WHERE id = ?`).get(id) as
+		{ path: string } | undefined;
+
+	if (!song) {
+		return reply.code(404).send("Song not found");
+	}
+
+	const stream = fs.createReadStream(song.path);
+	reply.type("audio/mpeg");
+	return reply.send(stream);
+});
+
+app.get("/albums", async () => {
+	const albums = db.prepare('SELECT album, artist, COUNT(*) as tracks FROM songs GROUP BY album, artist').all();
+	return albums;
+});
+
+app..get("/health", async () => {
+	return  {
+		status: "ok",
+		service: "Galleon",
+	};
+});
+
+
 app.listen({
 	port: 3000,
 	host: "0.0.0.0",
@@ -24,6 +63,9 @@ app.listen({
 	console.log(`Server listening at ${address}`);
 });
 
+}
+
+start();
 
 // to run server 
 // npx tsx src/index.ts
